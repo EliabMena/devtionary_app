22d12438b83c29856b36c8b4a8723f6948da4c73
Naturase <axel.saenz@utp.ac.pa>
2025-07-31 13:24:58 -0500

    Refactor category list to use FutureBuilder and icons
    Updated MainMenu and HorizontalCategoryList to use FutureBuilder for loading subcategories asynchronously and display category icons using images instead of text. This improves performance and visual consistency, and separates categories into Comandos, Instrucciones, and Términos sections. Also fixed navigation to the favorites screen.

--- lib/screens/main_menu.dart
+++ lib/screens/main_menu.dart
@@ -1,32 +1,19 @@
-import 'package:devtionary_app/widgets/tarjeta_categoria.dart';
 import 'package:devtionary_app/widgets/nav_button.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter/material.dart';
-import 'package:devtionary_app/db/db_models/subcategorias.dart';
 import 'dart:convert';
-import 'package:devtionary_app/screens/word_cards_screen.dart';
 import 'package:devtionary_app/Utility/thems/app_colors.dart';
 import 'package:devtionary_app/db/repositorios/subcategorias_repository.dart';
+import 'package:devtionary_app/widgets/lista_categorias.dart';
 
 class MainMenu extends StatelessWidget {
-  void logSubcategoriasLocales() async {
-    final subcategorias = await SubcategoriasRepository()
-        .fetchSubcategorias(); //Esto está llamando a la API constantemente, haciendo que crashee. Utiliza mejor el que llama a los datos de la bd local
-    print('Subcategorias locales:');
-    for (var sub in subcategorias) {
-      print(
-        'id: ${sub.id_subcategoria}, nombre: ${sub.nombre}, fecha_creacion: ${sub.fecha_creacion}, fecha_actualizacion: ${sub.fecha_actualizacion}',
-      );
-    }
-  }
-
   const MainMenu({super.key});
 
   Future<List<String>> obtenerRecientes() async {
     final prefs = await SharedPreferences.getInstance();
     return prefs.getStringList('recientes') ?? [];
   }
-
+  
   // Recupera la información de la palabra desde SharedPreferences (puedes guardar más info si lo necesitas)
   Future<Map<String, dynamic>?> obtenerInfoPalabra(String palabra) async {
     final prefs = await SharedPreferences.getInstance();
@@ -37,7 +24,6 @@ class MainMenu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    logSubcategoriasLocales();
     return Scaffold(
       backgroundColor: Colors.black, // Fondo oscuro
       appBar: AppBar(
@@ -140,225 +126,32 @@ class MainMenu extends StatelessWidget {
               ),
             ),
             SizedBox(height: 12),
-            FutureBuilder<List<Subcategorias>>(
-              future: SubcategoriasRepository()
-                  .fetchSubcategorias(), // Más de lo mismo, llamas a la API constantemente, lo que puede causar problemas de rendimiento
-              // Puedes cambiar esto para que use la base de datos local si necesitas los datos, así evitas que la api muera
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return Center(child: CircularProgressIndicator());
-                }
-                if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                  return Center(
-                    child: Text(
-                      'No hay categorías',
-                      style: TextStyle(color: Colors.white70),
-                    ),
-                  );
-                }
-                final subcategorias = snapshot.data!;
-                return SizedBox(
-                  width: 230,
-                  height: 230,
-                  child: SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    child: Row(
-                      children: List.generate(subcategorias.length, (index) {
-                        final cat = subcategorias[index];
-                        List<Color> gradient;
-                        Widget iconWidget;
-                        switch (cat.id_subcategoria.toString()) {
-                          case '1':
-                            gradient = [Color(0xFF7F00FF), Color(0xFFE100FF)];
-                            iconWidget = Text(
-                              'Docker',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 22,
-                              ),
-                            );
-                            break;
-                          case '2':
-                            gradient = [Color(0xFF005BEA), Color(0xFF00C6FB)];
-                            iconWidget = Text(
-                              'Git Bash',
-                              style: TextStyle(
-                                color: Colors.yellow,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 22,
-                              ),
-                            );
-                            break;
-                          case '3':
-                            gradient = [Color(0xFF00C6FB), Color(0xFF43E97B)];
-                            iconWidget = Text(
-                              'Linux',
-                              style: TextStyle(
-                                color: Colors.blue,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 22,
-                              ),
-                            );
-                            break;
-                          case '4':
-                            gradient = [Color(0xFF7F00FF), Color(0xFFE100FF)];
-                            iconWidget = Text(
-                              'MacOS',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 22,
-                              ),
-                            );
-                            break;
-                          case '5':
-                            gradient = [Color(0xFF005BEA), Color(0xFF00C6FB)];
-                            iconWidget = Text(
-                              'Windows',
-                              style: TextStyle(
-                                color: Colors.yellow,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 22,
-                              ),
-                            );
-                            break;
-                          case '6':
-                            gradient = [Color(0xFF00C6FB), Color(0xFF43E97B)];
-                            iconWidget = Text(
-                              'C++',
-                              style: TextStyle(
-                                color: Colors.blue,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 22,
-                              ),
-                            );
-                            break;
-                          case '7':
-                            gradient = [Color(0xFF7F00FF), Color(0xFFE100FF)];
-                            iconWidget = Text(
-                              'C#',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 22,
-                              ),
-                            );
-                            break;
-                          case '8':
-                            gradient = [Color(0xFF00C6FB), Color(0xFF43E97B)];
-                            iconWidget = Text(
-                              'Java',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 22,
-                              ),
-                            );
-                            break;
-                          case '9':
-                            gradient = [Color(0xFF7F00FF), Color(0xFFE100FF)];
-                            iconWidget = Text(
-                              'Javascript',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 22,
-                              ),
-                            );
-                            break;
-                          case '10':
-                            gradient = [Color(0xFF005BEA), Color(0xFF00C6FB)];
-                            iconWidget = Text(
-                              'Python',
-                              style: TextStyle(
-                                color: Colors.yellow,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 22,
-                              ),
-                            );
-                            break;
-                          case '11':
-                            gradient = [Color(0xFF00C6FB), Color(0xFF43E97B)];
-                            iconWidget = Text(
-                              'Desarrollo Web',
-                              style: TextStyle(
-                                color: Colors.blue,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 22,
-                              ),
-                            );
-                            break;
-                          case '12':
-                            gradient = [Color(0xFF7F00FF), Color(0xFFE100FF)];
-                            iconWidget = Text(
-                              'Herramientas y Desarrollo',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 22,
-                              ),
-                            );
-                            break;
-                          case '13':
-                            gradient = [Color(0xFF005BEA), Color(0xFF00C6FB)];
-                            iconWidget = Text(
-                              'Paradigmas de Programación',
-                              style: TextStyle(
-                                color: Colors.yellow,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 22,
-                              ),
-                            );
-                            break;
-                          case '14':
-                            gradient = [Color(0xFF00C6FB), Color(0xFF43E97B)];
-                            iconWidget = Text(
-                              'Programación de Sistemas',
-                              style: TextStyle(
-                                color: Colors.blue,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 22,
-                              ),
-                            );
-                            break;
-                          case '15':
-                            gradient = [Color(0xFF7F00FF), Color(0xFFE100FF)];
-                            iconWidget = Text(
-                              'Sistemas y Arquitectura',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 22,
-                              ),
-                            );
-                            break;
-                          default:
-                            gradient = [
-                              Colors.grey.shade800,
-                              Colors.grey.shade600,
-                            ];
-                            iconWidget = Icon(
-                              Icons.code,
-                              color: Colors.white,
-                              size: 28,
-                            );
-                        }
-                        return Container(
-                          width: 140,
-                          margin: EdgeInsets.only(right: 12),
-                          child: TarjetaCategoria(
-                            nombre: cat.nombre,
-                            gradient: gradient,
-                            icono: iconWidget,
-                          ),
-                        );
-                      }),
-                    ),
-                  ),
-
-                );
-              },
+            Column(
+              children: [
+                Text('Comandos'),
+                SizedBox( 
+                  width: double.infinity, 
+                  child: HorizontalCategoryList(
+                    subcategoriasFuture: SubcategoriasRepository().getSubcategoriasByCategoryId(1), 
+                  ), 
+                ),
+                SizedBox(height: 15),
+                Text('Instrucciones'),
+                SizedBox( 
+                  width: double.infinity, 
+                  child: HorizontalCategoryList(
+                    subcategoriasFuture: SubcategoriasRepository().getSubcategoriasByCategoryId(2), 
+                  ), 
+                ),
+                SizedBox(height: 15),
+                Text('Términos'),
+                SizedBox( 
+                  width: double.infinity, 
+                  child: HorizontalCategoryList(
+                    subcategoriasFuture: SubcategoriasRepository().getSubcategoriasByCategoryId(3), 
+                  ), 
+                ),
+              ],
             ),
             SizedBox(height: 32),
           ],
@@ -371,8 +164,8 @@ class MainMenu extends StatelessWidget {
             case 0:
               Navigator.pushNamed(
                 context,
-                '',
-              ); //falta implementación de la pantalla de favoritos
+                '/favoritos',
+              );
               break;
             case 1:
               Navigator.pushNamed(context, '/search');
--- lib/widgets/tarjeta_categoria.dart
+++ lib/widgets/tarjeta_categoria.dart
@@ -1,4 +1,3 @@
-// lib/widgets/categoria_card.dart
 import 'package:flutter/material.dart';
 
 class TarjetaCategoria extends StatelessWidget {
